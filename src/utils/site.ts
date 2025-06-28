fff4027606597be2fa57594ccf7829a4b67d7285
yuwigemane <yuwigemane@gmail.com>
2025-06-28 20:48:06 +0700

    Add files via upload

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,4 +1,4 @@
 export const nama = 'Bokep Terbaru Viral';
-export const desk = 'Video bokep indo jepang viral ngentot colmek bocil sampai tante.';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China Terbaru Viral';
 export const url = 'https://bokepterbaruviral.pages.dev';
-export const terbit = '2025-06-23T00:00:00Z';
+export const terbit = '2025-06-23T00:01:03Z';
